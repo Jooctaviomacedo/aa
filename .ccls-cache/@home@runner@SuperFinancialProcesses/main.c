86483d6cd584ba275332cb097e90ae3db2964d13
Joao Octavio <84680130+Jooctaviomacedo@users.noreply.github.com>
2022-11-22 16:34:04 +0000

    ss

--- .ccls-cache/@home@runner@SuperFinancialProcesses/main.c
+++ .ccls-cache/@home@runner@SuperFinancialProcesses/main.c
@@ -2,16 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-  int i;
-  char login[60], usuario[60], usuario1[60], senha[60], senha1[60], email[60],
+  int login;
+  char usuario[60], usuario1[60], senha[60], senha1[60], email[60],
       telefone[60];
 
   printf("Primeira vez aqui?: \n");
-  printf("Por Favor, faça o cadastro: \n");
+  printf("Por Favor, faça um pequeno cadastro antes: \n");
   printf("\n");
-
-  system("pause");
-  system("cls");
 
   printf("Nome de Usuario: \n");
   scanf("%s", usuario);
@@ -21,24 +18,41 @@ int main() {
   scanf("%s", email);
   printf("Nome de telefone: \n");
   scanf("%s", telefone);
-  system("pause");
 
   printf("Usuario: \n");
   scanf("%s", usuario1);
+  printf("Digite a Senha: ");
+  scanf("%s", senha1);
 
-  if (usuario == usuario) {
-    printf("Insira a senha");
-    scanf("%s", senha1);
-    if (senha == senha) {
-      printf("Usuario %s", usuario);
-      printf("email %s", email);
-      printf("telefone %s", telefone);
-      return 0;
-    } else {
-      printf("senha incorreta");
+  if (strcmp(usuario, usuario1) == 0 && strcmp(senha, senha1) == 0) {
+
+    printf("\n\n\t\tBem-vindo, %s!", usuario);
+    printf("\n");
+    printf("--------------------------------\n");
+    printf("\n");
+    printf("1- Minhas Informações\n");
+    printf("2- Alterar Informações\n");
+    printf("3- Sair\n");
+    scanf("%s", login);
+
+    switch (login) {
+    case 1:
+
+      while (login != 1) {
+        switch (login) {
+        case 2:
+
+          printf("Nome do usuario: %s\n", usuario);
+          printf("Senha: %s\n", senha);
+          printf("E-Mail: %s\n", email);
+          printf("Telefone: %s\n", usuario);
+          return 0;
+        }
+      }
     }
+
   } else {
-    printf("usuario incorreto");
-    return 0;
+
+    printf("\n\nDADOS INVALIDOS!\n\n");
   }
 }
--- main.c
+++ main.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-  int i;
-  char login[60], usuario[60], usuario1[60], senha[60], senha1[60], email[60],
+  int login;
+  char usuario[60], usuario1[60], senha[60], senha1[60], email[60],
       telefone[60];
 
   printf("Primeira vez aqui?: \n");
-  printf("Por Favor, faça o cadastro: \n");
+  printf("Por Favor, faça um pequeno cadastro antes: \n");
   printf("\n");
- 
 
   printf("Nome de Usuario: \n");
   scanf("%s", usuario);
@@ -22,20 +21,38 @@ int main() {
 
   printf("Usuario: \n");
   scanf("%s", usuario1);
+  printf("Digite a Senha: ");
+  scanf("%s", senha1);
 
-  if (usuario <= usuario1) {
-    printf("Insira a senha");
-    scanf("%s", senha1);
-    if (senha <= senha1) {
-      printf("Usuario: %s\n", usuario);
-      printf("email %s\n", email);
-      printf("telefone %s\n", telefone);
-      return 0;
-    } else {
-      printf("senha incorreta");
+  if (strcmp(usuario, usuario1) == 0 && strcmp(senha, senha1) == 0) {
+
+    printf("\n\n\t\tBem-vindo, %s!", usuario);
+    printf("\n");
+    printf("--------------------------------\n");
+    printf("\n");
+    printf("1- Minhas Informações\n");
+    printf("2- Alterar Informações\n");
+    printf("3- Sair\n");
+    scanf("%s", login);
+
+    switch (login) {
+    case 1:
+
+      while (login != 1) {
+        switch (login) {
+        case 2:
+
+          printf("Nome do usuario: %s\n", usuario);
+          printf("Senha: %s\n", senha);
+          printf("E-Mail: %s\n", email);
+          printf("Telefone: %s\n", usuario);
+          return 0;
+        }
+      }
     }
+
   } else {
-    printf("usuario incorreto");
-    return 0;
+
+    printf("\n\nDADOS INVALIDOS!\n\n");
   }
 }
