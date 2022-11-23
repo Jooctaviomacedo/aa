7d1d483db77bb89c19873e672764deaf54326ec8
Joao Octavio <84680130+Jooctaviomacedo@users.noreply.github.com>
2022-11-23 15:54:38 +0000

    ss

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main() {
-  int login;
-  char usuario[60], usuario1[60], senha[60], senha1[60], email[60],
-      telefone[60];
+  int i, opcao, opcao1, opcao3, user;
+
+  char usuario[10], usuario1[10], senha[8], senha1[8], email[20], telefone[11];
+  float menu[60];
 
   printf("Primeira vez aqui?: \n");
   printf("Por Favor, faça um pequeno cadastro antes: \n");
   printf("\n");
-
   printf("Nome de Usuario: \n");
   scanf("%s", usuario);
   printf("senha do Usuario: \n");
@@ -18,11 +18,13 @@ int main() {
   scanf("%s", email);
   printf("Nome de telefone: \n");
   scanf("%s", telefone);
+  printf("\e[1;1H\e[2J");
 
   printf("Usuario: \n");
   scanf("%s", usuario1);
   printf("Digite a Senha: ");
   scanf("%s", senha1);
+  printf("\e[1;1H\e[2J");
 
   if (strcmp(usuario, usuario1) == 0 && strcmp(senha, senha1) == 0) {
 
@@ -33,26 +35,47 @@ int main() {
     printf("1- Minhas Informações\n");
     printf("2- Alterar Informações\n");
     printf("3- Sair\n");
-    scanf("%s", login);
+    scanf("%d", &opcao1);
+    printf("\e[1;1H\e[2J");
+    if (opcao1 == 1) {
 
-    switch (login) {
-    case 1:
-
-      while (login != 1) {
-        switch (login) {
-        case 2:
-
-          printf("Nome do usuario: %s\n", usuario);
-          printf("Senha: %s\n", senha);
-          printf("E-Mail: %s\n", email);
-          printf("Telefone: %s\n", usuario);
-          return 0;
-        }
-      }
+      printf("________________\n");
+      printf("|-    ====    -|\n");
+      printf("|-  =-====-=  -|\n");
+      printf("|- C--O__O--D -|\n");
+      printf("|-  ---__---  -|\n");
+      printf("|-    ----    -|\n");
+      printf("----------------\n");
+      printf("\n");
+      printf("::Informações de Usuário::\n");
+      printf("\n");
+      printf("Nome do Usuario: %s\n", usuario);
+      printf("E-mail: %s\n", email);
+      printf("Telefone: %s\n", telefone);
+      printf("\n");
+      system("pause");
     }
 
-  } else {
+    if (opcao1 == 2) {
+      printf("::Alterar Informações de Usuário::\n");
+      printf("\n");
+      printf("Nome do Usuario: \n");
+      scanf("%s", usuario);
+      printf("senha: \n");
+      scanf("%s", senha);
+      printf("E-mail: \n");
+      scanf("%s", email);
+      printf("Telefone: \n");
+      scanf("%s", telefone);
 
+    } else {
+      printf("Saindo....");
+      exit(0);
+    }
+
+  }
+
+  else {
     printf("\n\nDADOS INVALIDOS!\n\n");
   }
 }
